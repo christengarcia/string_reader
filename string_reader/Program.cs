8ca73fba9badc2fb1c0843552e8e949ebdc7268c
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-16 22:43:04 +1100

    Add Main

--- string_reader/Program.cs
+++ string_reader/Program.cs
@@ -11,7 +11,20 @@ namespace string_reader
     {
         const string text = @"Prime offers algorithmic trading development programs";
         
+        static void Main()
+        {
+            using (StringReader reader = new StringReader(text))
+            {
+                int count = 0;
+                string textline;
+                while ((textline = reader.ReadLine()) != null)
+                {
+                    count++;
+                    Console.WriteLine("Line {0}: {1}", count, textline);
+                }
 
+                Console.ReadLine();
+            }
         }
     }
 }
