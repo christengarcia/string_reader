c731e4e5cc510a75767402b722357ce5efa8ab65
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-16 22:38:49 +1100

    First commit

---
+++ string_reader/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * C# Program to Demonstrate StringReader
+ */
+
+using System;
+using System.IO;
+
+namespace string_reader
+{
+    class Program
+    {
+        const string text = @"Prime offers algorithmic trading development programs";
+        
+
+        }
+    }
+}
